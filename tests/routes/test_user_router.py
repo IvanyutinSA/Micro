98f4b2586cee669e88aee2947436b361c8b65b41
IvanyutinSA <124494631+IvanyutinSA@users.noreply.github.com>
2026-03-28 11:42:07 +0600

    Delete tests directory

--- tests/routes/test_user_router.py
+++
@@ -1,9 +0,0 @@
-from test_utils.test_suit import TestSuit
-from src.main import create_server
-import src.routes.users_router
-import src.schemas
-
-
-class TestUserRouter(TestSuit):
-    def test_register(self):
-        pass
